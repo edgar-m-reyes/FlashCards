83739f5f7dd56009f19b3bf06cf530e8a7bd03ab
majineE <edgreyes@csumb.edu>
2018-10-14 00:23:14 -0700

    Finalized Project

--- app/src/main/java/com/example/edgarreyes/flashcards/MainActivity.java
+++ app/src/main/java/com/example/edgarreyes/flashcards/MainActivity.java
@@ -3,18 +3,62 @@ package com.example.edgarreyes.flashcards;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViewById(R.id.FlashCardQuestion).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.FlashCardAnswer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.FlashCardAnswer).setVisibility(View.VISIBLE);
-                findViewById(R.id.FlashCardQuestion).setVisibility(View.INVISIBLE);
+                findViewById(R.id.FlashCardAnswer1).setBackgroundColor(
+                    getResources().getColor(android.R.color.holo_red_light));
+                findViewById(R.id.FlashCardAnswer2).setBackgroundColor(
+                        getResources().getColor(android.R.color.holo_green_light));
+            }
+        });
+        findViewById(R.id.FlashCardAnswer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.FlashCardAnswer2).setBackgroundColor(
+                        getResources().getColor(android.R.color.holo_green_light));
+            }
+        });
+        findViewById(R.id.FlashCardAnswer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.FlashCardAnswer3).setBackgroundColor(
+                        getResources().getColor(android.R.color.holo_red_light));
+                findViewById(R.id.FlashCardAnswer2).setBackgroundColor(
+                        getResources().getColor(android.R.color.holo_green_light));
+            }
+        });
+        findViewById(R.id.rootView).setOnClickListener(new View.OnClickListener(){
+           @Override
+           public void onClick(View v) {
+               findViewById(R.id.FlashCardAnswer1).setBackgroundColor(
+                       getResources().getColor(R.color.originalColor));
+               findViewById(R.id.FlashCardAnswer2).setBackgroundColor(
+                       getResources().getColor(R.color.originalColor));
+               findViewById(R.id.FlashCardAnswer3).setBackgroundColor(
+                       getResources().getColor(R.color.originalColor));
+           }
+        });
+        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+            boolean isShowingAnswers = true;
+            @Override
+            public void onClick(View v) {
+                if (isShowingAnswers) {
+                    findViewById(R.id.FlashCardAnswer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.FlashCardAnswer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.FlashCardAnswer3).setVisibility(View.INVISIBLE);
+                    isShowingAnswers = false;
+                } else if (!isShowingAnswers) {
+                    findViewById(R.id.FlashCardAnswer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.FlashCardAnswer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.FlashCardAnswer3).setVisibility(View.VISIBLE);
+                    isShowingAnswers = true;
+                }
             }
         });
     }
